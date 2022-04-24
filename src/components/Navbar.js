dc94839cd25d41360db61aae703e643e703e0e1b
PJMantoss <joelptoss@gmail.com>
2022-04-24 23:11:17 +0100

    update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,10 +17,6 @@ const Navbar = () => {
                     <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
                     </li>
-                    <li class="nav-item dropdown">
-                    
-                    <a class="nav-link disabled">Disabled</a>
-                    </li>
                 </ul>
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
