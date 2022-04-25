76494bade5c59a48bc1e0285d580a24fb5066598
PJMantoss <joelptoss@gmail.com>
2022-04-25 13:43:18 +0100

    update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Home = () => {
   return (
     <div className='hero'>
-        <div class="card bg-dark text-white" style="width: 18rem;">
+        <div class="card bg-dark text-white"">
             <img src="..." class="card-img" alt="..." />
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
