7adec30ae0e2d8d672103c48dd871d7940069a0b
PJMantoss <joelptoss@gmail.com>
2022-04-25 14:20:35 +0100

    update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export const Home = () => {
     <div className='hero'>
         <div class="card bg-dark text-white border-0">
             <img src="./assets/bg.jpg" class="card-img" alt="Background" />
-            <div class="card-img-overlay d-flex">
+            <div class="card-img-overlay d-flex flex-column justify-content-center">
                 <div className='container'>
                     <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
                     <p class="card-text lead fs-2">CHECKOUT ALL THE TRENDS</p>
