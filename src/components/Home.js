ff0b85bb57b4578f4e816bd7b879cd95e3cda792
PJMantoss <joelptoss@gmail.com>
2022-04-25 14:15:32 +0100

    update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <div className='hero'>
         <div class="card bg-dark text-white border-0">
-            <img src="./assets/bg.jpg" class="card-img" alt="Background" height='550px' width='auto' />
+            <img src="./assets/bg.jpg" class="card-img" alt="Background" />
             <div class="card-img-overlay">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick up the bulk of the card's content.</p>
