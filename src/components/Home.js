0dfb37cf42a3740091218a4cb5dba012062a1994
PJMantoss <joelptoss@gmail.com>
2022-04-25 13:43:03 +0100

    update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const Home = () => {
   return (
     <div className='hero'>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="..." />
+        <div class="card bg-dark text-white" style="width: 18rem;">
+            <img src="..." class="card-img" alt="..." />
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
