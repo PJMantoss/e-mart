ec065c96dc2875b2bc35aab404582b1d8f092760
PJMantoss <joelptoss@gmail.com>
2022-04-25 14:16:12 +0100

    update Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,11 @@ export const Home = () => {
         <div class="card bg-dark text-white border-0">
             <img src="./assets/bg.jpg" class="card-img" alt="Background" />
             <div class="card-img-overlay">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick up the bulk of the card's content.</p>
-                <p class="card-text">Some quick up the bulk of the card's content.</p>
+                <div className='container'>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick up the bulk of the card's content.</p>
+                    <p class="card-text">Some quick up the bulk of the card's content.</p>
+                </div>
             </div>
         </div>
     </div>
