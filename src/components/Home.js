1e0b41b0e6bc6dc167a48d264221257dc17938a5
PJMantoss <joelptoss@gmail.com>
2022-04-26 17:55:29 +0100

    update Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Products from './Products';
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
                 </div>
             </div>
         </div>
+        <Products />
     </div>
   )
 }
