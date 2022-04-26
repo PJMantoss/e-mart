6df6966e9b780ca743ea40ca04944d7a3e412971
PJMantoss <joelptoss@gmail.com>
2022-04-26 19:11:46 +0100

    update Products

--- src/components/Products.js
+++ src/components/Products.js
@@ -39,13 +39,16 @@ const Products = () => {
 
   const ShowProducts = () => {
     return(
-      <div className='buttons d-flex justify-content-center mb-5 pb-5'>
-        <button className='btn btn-outline-dark'>All</button>
-        <button className='btn btn-outline-dark me-2'>Men's Clothing</button>
-        <button className='btn btn-outline-dark me-2'>Women's Clothing</button>
-        <button className='btn btn-outline-dark me-2'>Jewelry</button>
-        <button className='btn btn-outline-dark me-2'>Electronics</button>
-      </div>
+      <>
+          <div className='buttons d-flex justify-content-center mb-5 pb-5'>
+            <button className='btn btn-outline-dark'>All</button>
+            <button className='btn btn-outline-dark me-2'>Men's Clothing</button>
+            <button className='btn btn-outline-dark me-2'>Women's Clothing</button>
+            <button className='btn btn-outline-dark me-2'>Jewelry</button>
+            <button className='btn btn-outline-dark me-2'>Electronics</button>
+          </div>
+          {filter.map(product => {})}
+      </>
     );
   };
 
