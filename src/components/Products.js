ff24cc29594a6bbbf4926d95fe4b70416336f596
PJMantoss <joelptoss@gmail.com>
2022-04-26 17:33:49 +0100

    update Product

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 const Products = () => {
   const [data, setData] = useState([]);
+  const [] = useState();
   return (
     <div>Products</div>
   )
