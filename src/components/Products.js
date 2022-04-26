63974042d4d6d860159959dec06ec5825c940629
PJMantoss <joelptoss@gmail.com>
2022-04-26 17:35:41 +0100

    update Product

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,6 +4,7 @@ const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
+  
   return (
     <div>Products</div>
   )
