30b7a849270db4066ed28cd145a805bb0516de41
PJMantoss <joelptoss@gmail.com>
2022-04-26 17:35:33 +0100

    update Product

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(false);
   return (
     <div>Products</div>
   )
