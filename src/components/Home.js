6e690f2104fa67267d8848fd48c6d2f8de0787e6
PJMantoss <joelptoss@gmail.com>
2022-04-26 17:51:22 +0100

    update Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='hero'>
-        <div class="card bg-dark text-white border-0">
-            <img src="./assets/bg.jpg" class="card-img" alt="Background" />
-            <div class="card-img-overlay d-flex flex-column justify-content-center">
+        <div className="card bg-dark text-white border-0">
+            <img src="./assets/bg.jpg" className="card-img" alt="Background" />
+            <div className="card-img-overlay d-flex flex-column justify-content-center">
                 <div className='container'>
-                    <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-                    <p class="card-text lead fs-2">CHECKOUT ALL THE TRENDS</p>
+                    <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+                    <p className="card-text lead fs-2">CHECKOUT ALL THE TRENDS</p>
                 </div>
             </div>
         </div>
