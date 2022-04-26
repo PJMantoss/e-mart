92121799184f8fb8ca5af8756060614e3f66b71d
PJMantoss <joelptoss@gmail.com>
2022-04-26 17:53:52 +0100

    update Product

--- src/components/Products.js
+++ src/components/Products.js
@@ -33,7 +33,9 @@ const Products = () => {
     <div>
       <div className='container'>
         <div className='row'>
-          <div className='col-12'></div>
+          <div className='col-12'>
+            <h1>Latest Products</h1>
+          </div>
         </div>
       </div>
     </div>
