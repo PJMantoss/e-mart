f8147e16d2765c8c3e64c5ecb7306f75cdbfe228
PJMantoss <joelptoss@gmail.com>
2022-04-26 18:16:56 +0100

    update Products

--- src/components/Products.js
+++ src/components/Products.js
@@ -38,9 +38,11 @@ const Products = () => {
   };
 
   const ShowProducts = () => {
-    <div className='buttons'>
-      <button className='btn btn-outline-dark'></button>
-    </div>
+    return(
+      <div className='buttons'>
+        <button className='btn btn-outline-dark'></button>
+      </div>
+    );
   };
 
   return (
