91ba6bb1fc00a0e1df442d7d4fe17cb680f760d2
PJMantoss <joelptoss@gmail.com>
2022-04-26 17:59:00 +0100

    update Product

--- src/components/Products.js
+++ src/components/Products.js
@@ -34,7 +34,7 @@ const Products = () => {
       <div className='container my-5 py-5'>
         <div className='row'>
           <div className='col-12 mb-5'>
-            <h1>Latest Products</h1>
+            <h1 className='display-6 fw-bolder text-center'>Latest Products</h1>
           </div>
         </div>
       </div>
