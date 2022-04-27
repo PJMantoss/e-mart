bbd01ea56608ac11ca0589a09a6bda1dc85e5087
PJMantoss <joelptoss@gmail.com>
2022-04-27 17:09:18 +0100

    update Products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Skeleton from 'react-loading-skeleton';
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -32,7 +33,9 @@ const Products = () => {
   const Loading = () => {
     return(
       <>
-          <div className='col-md-3'></div>
+          <div className='col-md-3'>
+            <Skeleton />
+          </div>
       </>
     )
   };
