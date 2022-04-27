39eb50f6ff943b95231e3e4c00ef76c826ed7a1d
PJMantoss <joelptoss@gmail.com>
2022-04-27 17:45:55 +0100

    add Routes/Route in App.js.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/Home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
