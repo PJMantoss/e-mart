f4f232cc30d3d9b31abad272577d51d7ce5177ac
PJMantoss <joelptoss@gmail.com>
2022-04-27 15:50:56 +0100

    update Products

--- src/components/Products.js
+++ src/components/Products.js
@@ -54,7 +54,7 @@ const Products = () => {
                   <div class="card">
                     <img src={product.image} class="card-img-top" alt={product.title} />
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
+                      <h5 class="card-title">{product.title}</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                       <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
