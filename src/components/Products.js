c08d93448ac8f898df12a30c9369444b993a80df
PJMantoss <joelptoss@gmail.com>
2022-04-27 15:48:52 +0100

    update Products

--- src/components/Products.js
+++ src/components/Products.js
@@ -50,7 +50,16 @@ const Products = () => {
           {filter.map(product => {
             return(
               <>
-                <div className='col-md-3'></div>
+                <div className='col-md-3'>
+                  <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                </div>
               </>
             )
           })}
