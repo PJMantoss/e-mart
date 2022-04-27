b5397201410747ada1fe5860297a7de0a4f9dc2a
PJMantoss <joelptoss@gmail.com>
2022-04-27 17:50:00 +0100

    add Routes/Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/Home';
+import Products from './components/Products';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Navbar />
     <Routes>
-      <Route exact path="" element={} />
-      <Route exact path="" element={} />
-      <Navbar />
-      <Home />
+      <Route exact path="" element={<Home />} />
+      <Route path="" element={<Products />} />
     </Routes>
     </>
   );
