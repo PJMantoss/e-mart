2759295bb42a831e61466b6e45afd087e77257af
PJMantoss <joelptoss@gmail.com>
2022-04-28 14:51:09 +0100

    update single Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -25,7 +25,7 @@ const Product = () => {
         <div className='col-md-6'>
           <Skeleton height={400} />
         </div>
-        <div className='col-md-6'>
+        <div className='col-md-6' style={{lineHeight:2}}>
           <Skeleton height={50} width={300} />
           <Skeleton height={75} />
           <Skeleton height={25} width={150} />
