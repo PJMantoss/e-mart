42acd26160904e8568dbbd7a7fc8f8ea2b561aec
PJMantoss <joelptoss@gmail.com>
2022-04-28 09:28:54 +0100

    update single Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -52,7 +52,7 @@ const Product = () => {
           <button className='btn btn-outline-dark px-4 py-2'>
             Add to Cart
           </button>
-          <button className='btn btn-dark'>
+          <button className='btn btn-dark ms-2 px-3 py-2'>
             Go to Cart
           </button>
         </div>
