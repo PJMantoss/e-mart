04797b501bfbee7b4262d9b7a62c2803044267c5
PJMantoss <joelptoss@gmail.com>
2022-04-28 08:49:34 +0100

    update single Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Product = () => {
+  const [product, setProduct] = useState([]);
+
   return (
     <div>Product</div>
   )
