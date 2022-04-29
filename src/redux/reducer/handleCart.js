0121865c98ee4282ead21fded43fe8525f82996c
PJMantoss <joelptoss@gmail.com>
2022-04-29 14:01:48 +0100

    update handleCart

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -25,7 +25,9 @@ const handleCart = (state = cart, action) => {
             if(exist1.qty === 1){
                 return state.filter(x => x.id !== exist1.id)
             }else{
-                return state.map();
+                return state.map(
+                    x => x.id === product.id ? {...x, qty: x.qty - 1} : x
+                );
             }
            break;
         default:
