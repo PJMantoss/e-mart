8e223eda11edd5a506c6226cac37a9f90210cd3b
PJMantoss <joelptoss@gmail.com>
2022-04-29 13:47:55 +0100

    update handleCart

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -4,6 +4,7 @@ const handleCart = (state = cart, action) => {
     const product = action.payload;
     switch(action.type){
         case ADDITEM:
+            //Check if Product Already Exist
            return;
            break;
         case DELETEITEM:
