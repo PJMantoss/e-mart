3067442d3cb690828d75532cc870bc9b61fe2e37
PJMantoss <joelptoss@gmail.com>
2022-04-29 14:37:14 +0100

    update Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -68,7 +68,10 @@ const Product = () => {
             $ {product.price}
           </h3>
           <p className='lead'>{product.description}</p>
-          <button className='btn btn-outline-dark px-4 py-2'>
+          <button 
+            className='btn btn-outline-dark px-4 py-2'
+            onClick={() => addProduct(product)}
+          >
             Add to Cart
           </button>
           <NavLink to="/cart" className='btn btn-dark ms-2 px-3 py-2'>
