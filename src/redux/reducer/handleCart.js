8452daeb351c03cf99bdbb92141c983fcd424f19
PJMantoss <joelptoss@gmail.com>
2022-04-29 13:46:51 +0100

    update handleCart

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -3,10 +3,10 @@ const cart = [];
 const handleCart = (state = cart, action) => {
     const product = action.payload;
     switch(action.type){
-        case :
+        case ADDITEM:
            return;
            break;
-        case :
+        case DELETEITEM:
            return;
            break;
         default:
