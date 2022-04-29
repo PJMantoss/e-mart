418c6be1b9d6727f60e9bc1eb98ec48eb9d38490
PJMantoss <joelptoss@gmail.com>
2022-04-29 14:46:49 +0100

    update Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Cart = () => {
   return (
     <div>
-        <div className=''>
-            <div className=''></div>
+        <div className='row'>
+            <div className='col-md-4'></div>
+            <div className='col-md-4'></div>
         </div>
     </div>
   )
