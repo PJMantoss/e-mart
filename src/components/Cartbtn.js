e3ce6a592c0be382f8148668f486ba058994cbd4
PJMantoss <joelptoss@gmail.com>
2022-04-30 16:47:38 +0100

    update Cart btn

--- src/components/Cartbtn.js
+++ src/components/Cartbtn.js
@@ -6,7 +6,7 @@ const Cartbtn = () => {
   return (
     <>
         <NavLink to="/cart" className="btn btn-outline-primary ms-2">
-            <span className=''></span> Cart (0)
+            <span className='fa fa-shopping-cart me-1'></span> Cart (0)
         </NavLink>
     </>
   )
