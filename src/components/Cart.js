5ad7701928e1191e47220cdfedc0575b708ccd01
PJMantoss <joelptoss@gmail.com>
2022-05-01 14:41:06 +0100

    update Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -31,7 +31,30 @@ const Cart = () => {
 
     const cartItems = product => {
         return(
-            <></>
+            <>
+            <div className='row'>
+            <div className='col-md-4'>
+                <img 
+                    src={product.image} 
+                    alt={product.title} 
+                    height="200px" 
+                    width="180px" 
+                />
+            </div>
+            <div className='col-md-4'>
+                <h3> {product.title} </h3>
+                <p className='lead fw-bold'>
+                    {product.qty} X ${product.price} = ${product.qty * product.price}
+                </p>
+                <button className='btn btn-otline-dark me-4' onClick={() => handleButton(product)}>
+                    <i className='fa fa-minus'></i>
+                </button>
+                <button className='btn btn-otline-dark' onClick={() => handleButton(product)}>
+                    <i className='fa fa-plus'></i>
+                </button>
+            </div>
+        </div>
+            </>
         )
     };
 
