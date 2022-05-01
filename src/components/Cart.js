824d41366d966a8406d4632e6727c6a63509172d
PJMantoss <joelptoss@gmail.com>
2022-05-01 14:53:05 +0100

    update Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -68,7 +68,17 @@ const Cart = () => {
         )
     };
 
-    const buttons = () => {};
+    const buttons = () => {
+        return(
+            <div className='container'>
+                <div className='row'>
+                    <NavLink to='/checkout' className='btn btn-outline-dark mb-5 w-25 mx-auto'>
+                        Proceed To Checkout
+                    </NavLink>
+                </div>
+            </div>
+        );
+    };
 
   return (
     <div>
