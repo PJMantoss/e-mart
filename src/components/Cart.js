06e06775f6749a5b3c3a1c4324c12c5617683ac6
PJMantoss <joelptoss@gmail.com>
2022-05-01 14:29:21 +0100

    update Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { addCart, delCart } from '../redux/action/index';
 
 const Cart = () => {
   return (
