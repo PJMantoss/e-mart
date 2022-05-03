af6e6f659bf2373549d15e03ae0fe5e7fcd16307
PJMantoss <joelptoss@gmail.com>
2022-05-03 19:26:59 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/e-mart">
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
