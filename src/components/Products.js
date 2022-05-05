b4fd6c94a48fd75996b6ec209cadcef8bb7f764b
PJMantoss <joelptoss@gmail.com>
2022-05-05 18:53:19 +0100

    update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -60,7 +60,7 @@ const filterProduct = (cat) => {
   const ShowProducts = () => {
     return(
       <>
-          <div className='container d-flex mx-auto align-items-center mb-5 pb-5'>
+          <div className='container d-flex justify-content-centermb-5 pb-5'>
 
             <button className='btn btn-outline-dark me-2' onClick={() => setFilter(data)}>All</button>
             
