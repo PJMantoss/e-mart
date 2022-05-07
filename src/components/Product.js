8fc1b09466989af764fded54539fb3c97b959130
PJMantoss <joelptoss@gmail.com>
2022-05-07 18:43:07 +0100

    update styling for product

--- src/components/Product.js
+++ src/components/Product.js
@@ -61,7 +61,7 @@ const Product = () => {
     return(
       <>
         <div className='col-md-6'>
-          <img src={product.image} alt={product.title} height="400px" width="400px" />
+          <img className='img-fluid' src={product.image} alt={product.title} height="400px" width="400px" />
         </div>
         <div className='col-md-6'>
           <h4 className='text-uppercase text-black-50'>
