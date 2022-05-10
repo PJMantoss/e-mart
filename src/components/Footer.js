91bff8e387df3eec4de035d21b68bb124734a2fd
PJMantoss <joelptoss@gmail.com>
2022-05-10 14:10:08 +0100

    update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,60 +6,60 @@ const Footer = () => {
         <footer>
         <div class="container">
             <div class="row">
-            <div class="col-md-4 footer-column">
-                <ul class="nav flex-column">
-                <li class="nav-item">
-                    <span class="footer-title">Product</span>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Product 1</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Product 2</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Plans & Prices</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Frequently asked questions</a>
-                </li>
-                </ul>
-            </div>
-            <div class="col-md-4 footer-column">
-                <ul class="nav flex-column">
-                <li class="nav-item">
-                    <span class="footer-title">Company</span>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Job postings</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">News and articles</a>
-                </li>
-                </ul>
-            </div>
-            <div class="col-md-4 footer-column">
-                <ul class="nav flex-column">
+                <div class="col-md-4 footer-column">
+                    <ul class="nav flex-column">
                     <li class="nav-item">
-                        <span class="footer-title">Contact & Support</span>
+                        <span class="footer-title">Product</span>
                     </li>
                     <li class="nav-item">
-                        <span class="nav-link"><i class="fa fa-phone" aria-hidden="true"></i> +47 45 80 80 80</span>
+                        <a class="nav-link" href="#">Product 1</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fa fa-comments" aria-hidden="true"></i> Live chat</a>
+                        <a class="nav-link" href="#">Product 2</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i> Contact us</a>
+                        <a class="nav-link" href="#">Plans & Prices</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fa fa-star" aria-hidden="true"></i> Give feedback</a>
+                        <a class="nav-link" href="#">Frequently asked questions</a>
                     </li>
-                </ul>
-            </div>
+                    </ul>
+                </div>
+                <div class="col-md-4 footer-column">
+                    <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <span class="footer-title">Company</span>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Job postings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News and articles</a>
+                    </li>
+                    </ul>
+                </div>
+                <div class="col-md-4 footer-column">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <span class="footer-title">Contact & Support</span>
+                        </li>
+                        <li class="nav-item">
+                            <span class="nav-link"><i class="fa fa-phone" aria-hidden="true"></i> +47 45 80 80 80</span>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-comments" aria-hidden="true"></i> Live chat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i> Contact us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-star" aria-hidden="true"></i> Give feedback</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
             <div class="text-center">
