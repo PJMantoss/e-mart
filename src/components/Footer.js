5fec00e9b42a18f9ac7dfb9bb87328a93bb78f89
PJMantoss <joelptoss@gmail.com>
2022-05-10 13:47:14 +0100

    update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -72,7 +72,7 @@ const Footer = () => {
             <div class="col-md-4 box">
                 <ul class="list-inline social-buttons">
                     <li class="list-inline-item">
-                        <a href="#">
+                        <a href="3">
                             <i class="fa fa-twitter" aria-hidden="true"></i>
                         </a>
                     </li>
