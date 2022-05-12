80ad679e3bebc529d1d9f578412817de5325a6e0
PJMantoss <joelptoss@gmail.com>
2022-05-12 18:53:31 +0100

    update Cart styling

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -73,7 +73,7 @@ const Cart = () => {
         return(
             <div className='container'>
                 <div className='row'>
-                    <NavLink to='/checkout' className='btn btn-outline-dark mb-5 w-20 mx-auto'>
+                    <NavLink to='/checkout' className='btn btn-outline-dark mb-5 w-25 mx-auto'>
                         Proceed To Checkout
                     </NavLink>
                 </div>
