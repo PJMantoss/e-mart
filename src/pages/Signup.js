01dcde8fa50b343090c02d8373484e5634982094
PJMantoss <joelptoss@gmail.com>
2022-08-04 14:37:56 +0100

    update Signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='container'>
+      <div className='wrapper'>
+        <Title>CREATE AN ACCOUNT</Title>
+        <form>
+          <input placeholder="first name" />
+          <input placeholder="last name" />
+          <input placeholder="email" />
+          <input placeholder="username" />
+          <input placeholder="password" />
+          <input placeholder="confirm password" />
+          <Agreement>
+            By creating an account, I agree to the processing of my personal data
+            in accordance with the <b>Privacy Policy</b>.
+          </Agreement>
+          <Button>CREATE</Button>
+        </form>
+      </div>
+    </div>
   )
 }
 
