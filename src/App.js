ff1eb38016fedf577f9242717c63326a96811197
PJMantoss <joelptoss@gmail.com>
2022-08-04 14:40:21 +0100

    update Signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Products from './pages/Products';
 import Product from './components/Product';
 import { Routes, Route } from 'react-router-dom';
 import Cart from './pages/Cart';
+import Signup from './pages/Signup';
 import Footer from './components/Footer';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/products" element={<Products />} />
       <Route path="/products/:id" element={<Product />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
     <Footer />
     </>
