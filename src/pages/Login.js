5c1aaf2124e375d5d618cbed26a9d46d68438fe6
PJMantoss <joelptoss@gmail.com>
2022-08-06 19:06:13 +0100

    update Login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
           <label for="exampleInputPassword1" className="form-label">Password</label>
           <input type="password" className="form-control" id="exampleInputPassword1" />
         </div>
-        <button type="submit" className="btn btn-primary">Create</button>
+        <button type="submit" className="btn btn-primary">Login</button>
       </form>
     </>
   )
