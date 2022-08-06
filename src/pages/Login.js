646d65d1f8c0c95fab66734d8a3f2b0bd59a5924
PJMantoss <joelptoss@gmail.com>
2022-08-06 19:05:23 +0100

    update Login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,8 @@ import React from 'react'
 const Login = () => {
   return (
     <>
-      <h1 className='text-center my-4'>CREATE AN ACCOUNT</h1>
+      <h1 className='text-center my-4'>Log In</h1>
       <form className="w-75 mx-auto mb-5">
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">Name</label>
-          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        </div>
         <div className="mb-3">
           <label for="exampleInputEmail1" className="form-label">Email address</label>
           <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
